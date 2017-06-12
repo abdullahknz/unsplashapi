6c7d0ca21448d26b61a3d51da3bfb01e5a2ee340
sbaidachni <abdullahknz@gmail.com>
2017-06-12 05:36:58 +0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+//provider classımızı dahil ediyoruz
+import { UnsplashServiceProvider } from '../../providers/unsplash-service/unsplash-service';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [UnsplashServiceProvider]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  //datayı set edebileceğimiz bir değişken oluşturuyoruz.
+  public images: any;
 
+  constructor(public unsplashService: UnsplashServiceProvider /* servisimizi constructor metodumuza tanımlıyoruz */) {}
+
+   //fonksiyonumuzu ekranımız yüklenirken çalıştırıyoruz.
+  ionViewDidLoad(){
+    this.loadImages();
   }
+  loadImages(){
+    //fonksiyonumuzu provider classımızdan çağırıp istek yapmasını istiyoruz.
+    this.unsplashService.load().then(data => {
+      this.images = data;
+      console.log(data);
+    });
+    
+  }
+
 
 }
---
+++ src/providers/unsplash-service/unsplash-service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class UnsplashServiceProvider {
+
+  data:any;
+  constructor(public http: Http) {
+   
+  }
+
+  load() {
+    //client_id yani app id set etmemiz gerekiyor.
+    var client_id = 'f8d4dfac644066a84bf50ad3070ca9bba0d13698a487200f048ecf0e1d0b6fe4'
+  
+  if (this.data) {
+    // datamız var mı diye kontrol ediyoruz.
+    return Promise.resolve(this.data);
+  }
+
+  // henüz data yok ise bu kısım çalışır
+  return new Promise(resolve => {
+    // Burada api ya istek göndermek için Angular HTTP provider kullanıyoruz.
+    // eğer data dönerse Json datamızı Json parse edip bir JSON objesi oluşturuyoruz.
+    this.http.get('https://api.unsplash.com/photos/?per_page=30&client_id='+client_id)
+      .map(res => res.json())
+      .subscribe(data => {
+        // eğer istek başarılı olursa dönen JSON verisini data değişkenine set ediyoruz.
+        this.data = data;
+        resolve(this.data);
+      });
+  });
+}
+}
